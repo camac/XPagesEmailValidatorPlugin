0e1f365c21e507f123ae5bb754bc71f87cead800
Cameron Gregor <camerongregor@gmail.com>
2020-02-18 15:43:20 +1100

    Extend AbstractValidator so that the message property is available

--- com.gregorbyte.xsp.emailvalidator/src/com/gregorbyte/xsp/validator/EmailAddressValidator.java
+++ com.gregorbyte.xsp.emailvalidator/src/com/gregorbyte/xsp/validator/EmailAddressValidator.java
@@ -10,25 +10,28 @@ import javax.faces.validator.Validator;
 import javax.faces.validator.ValidatorException;
 
 import com.ibm.commons.util.StringUtil;
+import com.ibm.xsp.validator.AbstractValidator;
 
-public class EmailAddressValidator implements Validator {
+public class EmailAddressValidator extends AbstractValidator {
 
 	private final static String EMAIL_PATTERN = "[^\\s@]+@[^\\s@]+\\.[^\\s@]+";
-	private final static Pattern EMAIL_COMPILED_PATTERN = Pattern
-			.compile(EMAIL_PATTERN);
+	private final static Pattern EMAIL_COMPILED_PATTERN = Pattern.compile(EMAIL_PATTERN);
 
-	public void validate(FacesContext context, UIComponent component, Object o)
-			throws ValidatorException {
+	public void validate(FacesContext context, UIComponent component, Object o) throws ValidatorException {
 		if (o == null || StringUtil.isEmpty((String) o)) {
 			return;
 		}
 		String trimmed = ((String) o).trim();
 		Matcher matcher = EMAIL_COMPILED_PATTERN.matcher(trimmed);
 		if (!matcher.matches()) {
-			FacesMessage msg = new FacesMessage("Invalid email value!",
-					"Supplied Email address is not in a valid format");
-			msg.setSeverity(FacesMessage.SEVERITY_ERROR);
-			throw new ValidatorException(msg);
+
+			String msgText = getMessage();
+			if (StringUtil.isEmpty(msgText)) {
+				msgText = "The supplied Email address is not in a valid format";
+			}
+
+			throw createValidatorEx(msgText);
+			
 		}
 	}
 
